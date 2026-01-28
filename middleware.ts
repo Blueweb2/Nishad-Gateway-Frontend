f75ed97e16d7e549084f1f33c545bd24a858673a
Sabithabasima <sabithabasimavk@gmail.com>
2026-01-28 17:36:11 +0530

    fix(admin): adjust middleware auth handling

--- middleware.ts
+++ middleware.ts
@@ -1,22 +1,60 @@
+// import { NextRequest, NextResponse } from "next/server";
+
+// export function middleware(req: NextRequest) {
+//   const { pathname } = req.nextUrl;
+
+//   const access = req.cookies.get("admin_access_token")?.value;
+//   const refresh = req.cookies.get("admin_refresh_token")?.value;
+
+//   // if no tokens -> block all admin pages except login
+//   if (!access && !refresh && pathname.startsWith("/admin") && pathname !== "/admin/login") {
+//     const url = req.nextUrl.clone();
+//     url.pathname = "/admin/login";
+//     return NextResponse.redirect(url);
+//   }
+
+//   // if token exists -> prevent opening login page
+//   if ((access || refresh) && pathname === "/admin/login") {
+//     const url = req.nextUrl.clone();
+//     url.pathname = "/admin/dashboard";
+//     return NextResponse.redirect(url);
+//   }
+
+//   return NextResponse.next();
+// }
+
+// export const config = {
+//   matcher: ["/admin/:path*"],
+// };
+
+
+
+
+
+
+import { log } from "console";
 import { NextRequest, NextResponse } from "next/server";
+import { cookies } from "next/headers";
 
 export function middleware(req: NextRequest) {
   const { pathname } = req.nextUrl;
 
-  const access = req.cookies.get("admin_access_token")?.value;
-  const refresh = req.cookies.get("admin_refresh_token")?.value;
 
-  // if no tokens -> block all admin pages except login
-  if (!access && !refresh && pathname.startsWith("/admin") && pathname !== "/admin/login") {
-    const url = req.nextUrl.clone();
-    url.pathname = "/admin/login";
-    return NextResponse.redirect(url);
+
+  const access = req.cookies.get("admin_access_token");
+
+  console.log("this is access",access);
+  
+
+  // allow login page
+  if (pathname === "/admin/login") {
+    return NextResponse.next();
   }
 
-  // if token exists -> prevent opening login page
-  if ((access || refresh) && pathname === "/admin/login") {
+  // protect admin routes
+  if (!access && pathname.startsWith("/admin")) {
     const url = req.nextUrl.clone();
-    url.pathname = "/admin/dashboard";
+    url.pathname = "/admin/login";
     return NextResponse.redirect(url);
   }
 
