d1629f0f635984061771200d18ae69ec73833515
Sabithabasima <sabithabasimavk@gmail.com>
2026-02-04 16:23:41 +0530

    fix: update api barrel exports after admin/public refactor

--- lib/api/index.ts
+++ lib/api/index.ts
@@ -5,7 +5,5 @@ export * from "./error";
 
 // existing exports
 export * from "./adminAuth.api";
-export * from "./services.api";
-export * from "./subservices.api";
 export * from "./content.api";
 export * from "./upload.api";
