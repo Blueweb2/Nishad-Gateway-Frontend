682d8a26ea9feca5c24df17b7c37d25405cd808a
Sabithabasima <sabithabasimavk@gmail.com>
2026-02-06 11:00:30 +0530

    refactor(http): switch request layer to publicAxios and clean axios structure

--- lib/api/request.ts
+++ lib/api/request.ts
@@ -1,8 +1,8 @@
-import { api } from "@/lib/axios";
+import { publicAxios } from "@/lib/http/publicAxios";
 import { AxiosRequestConfig } from "axios";
 
 export const getData = async <T = any>(url: string, config?: AxiosRequestConfig) => {
-  const res = await api.get(url, config);
+  const res = await publicAxios.get(url, config);
   return res.data as T;
 };
 
@@ -11,7 +11,7 @@ export const postData = async <T = any>(
   payload?: any,
   config?: AxiosRequestConfig
 ) => {
-  const res = await api.post(url, payload, config);
+  const res = await publicAxios.post(url, payload, config);
   return res.data as T;
 };
 
@@ -20,11 +20,11 @@ export const putData = async <T = any>(
   payload?: any,
   config?: AxiosRequestConfig
 ) => {
-  const res = await api.put(url, payload, config);
+  const res = await publicAxios.put(url, payload, config);
   return res.data as T;
 };
 
 export const deleteData = async <T = any>(url: string, config?: AxiosRequestConfig) => {
-  const res = await api.delete(url, config);
+  const res = await publicAxios.delete(url, config);
   return res.data as T;
 };
