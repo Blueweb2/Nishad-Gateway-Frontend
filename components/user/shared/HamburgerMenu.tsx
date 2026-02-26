988d4c8937a5e232e43f24572da13fc41e6b1f41
navaskm <navaskm228@gmail.com>
2026-02-26 12:44:00 -0800

    add animation in hamburger menue

--- components/user/shared/HamburgerMenu.tsx
+++ components/user/shared/HamburgerMenu.tsx
@@ -1,9 +1,8 @@
 "use client";
 
-import { useEffect, useState } from "react";
+import { useEffect, useState, useRef } from "react";
 import Link from "next/link";
 import { motion } from "framer-motion";
-import { usePathname } from "next/navigation";
 import { ArrowRight } from "lucide-react";
 import { useRouter } from "next/navigation";
 import { getServicesMenu } from "@/lib/api/public/services.api";
@@ -39,32 +38,32 @@ export default function HamburgerMenu({ open, onClose }: Props) {
   const [useDarkText, setUseDarkText] = useState(false);
   const MotionLink = motion(Link);
 
-  const pathname = usePathname();
+  const hasAnimated = useRef(false);
 
   const router = useRouter();
-const [services, setServices] = useState<ServiceItem[]>([]);
-const [expanded, setExpanded] = useState<string | null>(null);
+  const [services, setServices] = useState<ServiceItem[]>([]);
+  const [expanded, setExpanded] = useState<string | null>(null);
 
 
-useEffect(() => {
-  if (!open) return;
+  useEffect(() => {
+    if (!open) return;
 
-  const fetchServices = async () => {
-    try {
-      const res = await getServicesMenu();
-      const data = res?.data || [];
-      setServices(data);
+    const fetchServices = async () => {
+      try {
+        const res = await getServicesMenu();
+        const data = res?.data || [];
+        setServices(data);
 
-      if (data.length > 0) {
-        setExpanded(data[0]._id);
+        if (data.length > 0) {
+          setExpanded(data[0]._id);
+        }
+      } catch (error) {
+        console.error("Failed to load services");
       }
-    } catch (error) {
-      console.error("Failed to load services");
-    }
-  };
+    };
 
-  fetchServices();
-}, [open]);
+    fetchServices();
+  }, [open]);
   // Mount animation
   useEffect(() => {
     if (open) {
@@ -136,143 +135,133 @@ useEffect(() => {
           bg-gradient-to-b from-white/40 to-white/25
           backdrop-blur-3xl
           shadow-[0_30px_90px_rgba(0,0,0,0.35)]
-          overflow-hidden
           will-change-transform
           isolate
         "
       >
-        <div className="px-6 py-8">
+        <div>
+          <div className="px-4 py-4 max-h-[70vh] overflow-y-auto mb-5 hide-scrollbar">
+            {/* SERVICES SECTION */}
+            <div className="space-y-4">
+              {services.map((service) => {
+                const isOpen = expanded === service._id;
 
-          {/* <div className="space-y-6">
-            {menu.map((item, idx) => (
-              <div key={idx}>
-                {item.href ? (
-                  <Link
-                    href={item.href}
-                    onClick={onClose}
-                    className={`block text-[20px] font-semibold ${textColor} hover:opacity-70 transition`}
-                  >
-                    {item.title}
-                  </Link>
-                ) : (
-                  <p className={`text-[20px] font-semibold ${textColor}`}>
-                    {item.title}
-                  </p>
-                )}
-
-                {item.children && (
-                  <ul className={`mt-2 space-y-1 text-sm ${subTextColor}`}>
-                    {item.children.map((c) => (
-                      <li key={c.href}>
-                        <Link
-                          href={c.href}
-                          onClick={onClose}
-                          className="hover:opacity-70 transition"
+                return (
+                  <div key={service._id} className="overflow-hidden">
+                    {/* SERVICE HEADER */}
+                    <div className="flex items-center justify-between ">
+                      <button
+                        onClick={() => setExpanded(isOpen ? null : service._id)}
+                        className="flex-1 text-left"
+                      >
+                        <motion.span
+                          initial={{ opacity: 0, y: 300 }}
+                          animate={{ opacity: 1, y: 0 }}
+                          transition={{ duration: 0.6, ease: "easeOut" }}
+                          className="text-xs text-gray-400 block"
                         >
-                          {c.label}
-                        </Link>
-                      </li>
-                    ))}
-                  </ul>
-                )}
-              </div>
-            ))}
-          </div> */}
+                          {service.index}
+                        </motion.span>
 
-          <div className="px-6 py-8">
+                        <motion.h2
+                          initial={{ opacity: 0, y: 300 }}
+                          animate={{ opacity: 1, y: 0 }}
+                          transition={{ duration: 0.6, ease: "easeOut" }}
+                          className={`text-[20px] font-semibold transition ${
+                            isOpen ? "text-teal-600" : textColor
+                          }`}
+                        >
+                          {service.title}
+                        </motion.h2>
+                      </button>
 
-  {/* SERVICES SECTION */}
-  <div className="space-y-4">
-    {services.map((service) => {
-      const isOpen = expanded === service._id;
+                      <button
+                        onClick={() => {
+                          onClose();
+                          router.push(`/services/${service.slug}`);
+                        }}
+                        className="w-9 h-9 rounded-lg border border-white/30 flex items-center justify-center"
+                      >
+                        <ArrowRight size={16} />
+                      </button>
+                    </div>
 
-      return (
-        <div key={service._id}>
-          {/* SERVICE HEADER */}
-          <div className="flex items-center justify-between">
-            <button
-              onClick={() => setExpanded(isOpen ? null : service._id)}
-              className="flex-1 text-left"
-            >
-              <span className="text-xs text-gray-400 block">
-                {service.index}
-              </span>
+                    {/* SUB SERVICES */}
+                    {isOpen && (
+                      <ul className={`mt-3 space-y-2 text-sm ${subTextColor}`}>
+                        {service.subServices?.map((sub) => (
+                          <motion.li
+                            initial={{ opacity: 0, y: 300 }}
+                            animate={{ opacity: 1, y: 0 }}
+                            transition={{ duration: 0.6, ease: "easeOut" }}
+                            key={sub._id}
+                            onClick={() => {
+                              onClose();
+                              router.push(
+                                `/services/${service.slug}/${sub.slug}`
+                              );
+                            }}
+                            className="cursor-pointer hover:text-teal-400 transition"
+                          >
+                            {sub.title}
+                          </motion.li>
+                        ))}
+                      </ul>
+                    )}
+                  </div>
+                );
+              })}
+            </div>
 
-              <h2
-                className={`text-[20px] font-semibold transition ${
-                  isOpen ? "text-teal-600" : textColor
-                }`}
+            {/* DIVIDER */}
+            <div className={`my-8 border-t ${borderColor}`} />
+
+            {/* STATIC LINKS */}
+            <div className="space-y-3">
+              <MotionLink
+                initial={!hasAnimated.current ? { opacity: 0, y: 300 } : false}
+                animate={{ opacity: 1, y: 0 }}
+                transition={{ duration: 0.6, ease: "easeOut" }}
+                onAnimationComplete={() => {
+                  hasAnimated.current = true;
+                }}
+                href="/about-us"
+                onClick={onClose}
+                className={`block text-[18px] font-semibold ${textColor} hover:opacity-70 transition`}
               >
-                {service.title}
-              </h2>
-            </button>
+                About us
+              </MotionLink>
 
-            <button
-              onClick={() => {
-                onClose();
-                router.push(`/services/${service.slug}`);
-              }}
-              className="w-9 h-9 rounded-lg border border-white/30 flex items-center justify-center"
-            >
-              <ArrowRight size={16} />
-            </button>
+              <MotionLink
+                initial={!hasAnimated.current ? { opacity: 0, y: 300 } : false}
+                animate={{ opacity: 1, y: 0 }}
+                transition={{ duration: 0.6, ease: "easeOut" }}
+                onAnimationComplete={() => {
+                  hasAnimated.current = true;
+                }}
+                href="/blogs"
+                onClick={onClose}
+                className={`block text-[18px] font-semibold ${textColor} hover:opacity-70 transition`}
+              >
+                Blog
+              </MotionLink>
+
+              <MotionLink
+                initial={!hasAnimated.current ? { opacity: 0, y: 300 } : false}
+                animate={{ opacity: 1, y: 0 }}
+                transition={{ duration: 0.6, ease: "easeOut" }}
+                onAnimationComplete={() => {
+                  hasAnimated.current = true;
+                }}
+                href="/contact"
+                onClick={onClose}
+                className={`block text-[18px] font-semibold ${textColor} hover:opacity-70 transition`}
+              >
+                Contacts
+              </MotionLink>
+            </div>
+
           </div>
-
-          {/* SUB SERVICES */}
-          {isOpen && (
-            <ul className={`mt-3 space-y-2 text-sm ${subTextColor}`}>
-              {service.subServices?.map((sub) => (
-                <li
-                  key={sub._id}
-                  onClick={() => {
-                    onClose();
-                    router.push(
-                      `/services/${service.slug}/${sub.slug}`
-                    );
-                  }}
-                  className="cursor-pointer hover:text-teal-400 transition"
-                >
-                  {sub.title}
-                </li>
-              ))}
-            </ul>
-          )}
-        </div>
-      );
-    })}
-  </div>
-
-  {/* DIVIDER */}
-  <div className={`my-8 border-t ${borderColor}`} />
-
-  {/* STATIC LINKS */}
-  <div className="space-y-5">
-    <Link
-      href="/about-us"
-      onClick={onClose}
-      className={`block text-[18px] font-semibold ${textColor} hover:opacity-70 transition`}
-    >
-      About us
-    </Link>
-
-    <Link
-      href="/blogs"
-      onClick={onClose}
-      className={`block text-[18px] font-semibold ${textColor} hover:opacity-70 transition`}
-    >
-      Blog
-    </Link>
-
-    <Link
-      href="/contact"
-      onClick={onClose}
-      className={`block text-[18px] font-semibold ${textColor} hover:opacity-70 transition`}
-    >
-      Contacts
-    </Link>
-  </div>
-
-</div>
         </div>
       </div>
     </div>
