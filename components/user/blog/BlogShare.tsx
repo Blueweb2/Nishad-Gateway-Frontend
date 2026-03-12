189ca135c4e38af5a813576eb19eaedc339f7490
navaskm <navaskm228@gmail.com>
2026-03-12 00:11:16 -0700

    set in to responsive blog page

--- components/user/blog/BlogShare.tsx
+++ components/user/blog/BlogShare.tsx
@@ -17,70 +17,113 @@ export default function BlogShare({ title }: Props) {
     const encodedUrl = encodeURIComponent(url);
     const encodedTitle = encodeURIComponent(title);
 
-    return (
-       <div className="mt-20 sticky top-32 self-start">
-            <div className="bg-white rounded-2xl py-8 px-6 flex flex-col items-center">
 
-                <div className="flex flex-col gap-4 sticky">
+
+    return (
+        <div className="lg:mt-20 lg:sticky lg:top-32 self-start">
+            <div className="
+            bg-white 
+            rounded-2xl 
+            lg:py-8 
+            lg:px-6 
+            flex 
+            flex-row lg:flex-col 
+            items-center 
+            lg:gap-4
+            ">
+
+                {/* SHARE ICONS */}
+                <div className="flex flex-row lg:flex-col gap-4">
+
                     {/* WhatsApp */}
                     <a
-                        href={`https://wa.me/?text=${encodedTitle}%20${encodedUrl}`}
-                        target="_blank"
-                        rel="noopener noreferrer"
-                        className="w-12 h-12 flex items-center justify-center
-                            rounded-xl border border-teal-500
-                            transition-transform duration-300
-                            hover:scale-110"        
+                    href={`https://wa.me/?text=${encodedTitle}%20${encodedUrl}`}
+                    target="_blank"
+                    rel="noopener noreferrer"
+                    className="
+                        w-10 h-10 lg:w-12 lg:h-12
+                        flex items-center justify-center
+                        rounded-xl
+                        border border-teal-500
+                        transition-transform duration-300
+                        hover:scale-110
+                    "
                     >
-                        <Image
-                            src="/icons/social/whatsapp.svg"
-                            alt="WhatsApp"
-                            width={20}
-                            height={20}
-                        />
+                    <Image
+                        src="/icons/social/whatsapp.svg"
+                        alt="WhatsApp"
+                        width={18}
+                        height={18}
+                    />
                     </a>
 
                     {/* Facebook */}
                     <a
-                        href={`https://www.facebook.com/sharer/sharer.php?u=${encodedUrl}`}
-                        target="_blank"
-                        rel="noopener noreferrer"
-                        className="w-12 h-12 flex items-center justify-center rounded-xl border border-teal-500 transition-transform duration-300 hover:scale-110"        >
-                        <Image
-                            src="/icons/social/fb.svg"
-                            alt="Facebook"
-                            width={12}
-                            height={12}
-                        />
+                    href={`https://www.facebook.com/sharer/sharer.php?u=${encodedUrl}`}
+                    target="_blank"
+                    rel="noopener noreferrer"
+                    className="
+                        w-10 h-10 lg:w-12 lg:h-12
+                        flex items-center justify-center
+                        rounded-xl
+                        border border-teal-500
+                        transition-transform duration-300
+                        hover:scale-110
+                    "
+                    >
+                    <Image
+                        src="/icons/social/fb.svg"
+                        alt="Facebook"
+                        width={14}
+                        height={14}
+                    />
                     </a>
 
-                    {/* Twitter/X */}
+                    {/* Twitter */}
                     <a
-                        href={`https://twitter.com/intent/tweet?url=${encodedUrl}&text=${encodedTitle}`}
-                        target="_blank"
-                        rel="noopener noreferrer"
-                        className="w-12 h-12 flex items-center justify-center rounded-xl border border-teal-500 transition-transform duration-300 hover:scale-110" >
-                        <Image
-                            src="/icons/social/tweet.svg"
-                            alt="Twitter"
-                            width={20}
-                            height={20}
-                        />
+                    href={`https://twitter.com/intent/tweet?url=${encodedUrl}&text=${encodedTitle}`}
+                    target="_blank"
+                    rel="noopener noreferrer"
+                    className="
+                        w-10 h-10 lg:w-12 lg:h-12
+                        flex items-center justify-center
+                        rounded-xl
+                        border border-teal-500
+                        transition-transform duration-300
+                        hover:scale-110
+                    "
+                    >
+                    <Image
+                        src="/icons/social/tweet.svg"
+                        alt="Twitter"
+                        width={18}
+                        height={18}
+                    />
                     </a>
 
                     {/* Email */}
                     <a
-                        href={`mailto:?subject=${encodedTitle}&body=${encodedUrl}`}
-                        className="w-12 h-12 flex items-center justify-center rounded-xl border border-teal-500 transition-transform duration-300 hover:scale-110" >     
-                        <Image
-                            src="/icons/social/at.svg"
-                            alt="Email"
-                            width={20}
-                            height={20}
-                        />
+                    href={`mailto:?subject=${encodedTitle}&body=${encodedUrl}`}
+                    className="
+                        w-10 h-10 lg:w-12 lg:h-12
+                        flex items-center justify-center
+                        rounded-xl
+                        border border-teal-500
+                        transition-transform duration-300
+                        hover:scale-110
+                    "
+                    >
+                    <Image
+                        src="/icons/social/at.svg"
+                        alt="Email"
+                        width={18}
+                        height={18}
+                    />
                     </a>
+
                 </div>
+
             </div>
-       </div> 
+        </div>
     );
 }
