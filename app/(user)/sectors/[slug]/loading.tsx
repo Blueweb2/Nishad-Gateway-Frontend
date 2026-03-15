f66b052e4d34f87dd68208ab3c8c3689d91c5ec1
navaskm <navaskm228@gmail.com>
2026-03-14 22:09:02 -0700

    updated sectors page loading spinner

--- app/(user)/sectors/[slug]/loading.tsx
+++ app/(user)/sectors/[slug]/loading.tsx
@@ -1,15 +1,13 @@
 'use client'
 
-const loading = () => {
+export default function Loading() {
   return (
-    <div className="min-h-screen bg-white flex items-center justify-center">
+    <div className="fixed inset-0 z-50 bg-white flex items-center justify-center">
       <div className="flex gap-2">
         <div className="w-3 h-3 bg-gray-600 rounded-full animate-bounce"></div>
         <div className="w-3 h-3 bg-gray-600 rounded-full animate-bounce [animation-delay:.2s]"></div>
         <div className="w-3 h-3 bg-gray-600 rounded-full animate-bounce [animation-delay:.4s]"></div>
       </div>
     </div>
-  )
+  );
 }
-
-export default loading;
