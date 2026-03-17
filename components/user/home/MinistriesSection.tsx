d45512fef517e2bf8aa87c3cf2f638989f49cd88
navaskm <navaskm228@gmail.com>
2026-03-17 01:52:06 -0700

    fix responsive issue in minstries section

--- components/user/home/MinistriesSection.tsx
+++ components/user/home/MinistriesSection.tsx
@@ -16,8 +16,8 @@ let ministries: Ministry[] = [];
   if (!ministries?.length) return null;
 
   return (
-    <section  id="ministries" className="w-full bg-black text-white py-28 mx-auto px-6 flex items-center justify-end" data-navbar="light">
-      <div className="max-w-[1320px] px-6">
+    <section  id="ministries" className="w-full bg-black text-white py-28 mx-auto px-6 max-w-[1320px] lg:max-w-full lg:flex lg:items-center lg:justify-end" data-navbar="light">
+      <div className="max-w-[1320px] mx-auto lg:mx-0 px-6">
 
         {/* HEADER */}
         <div className="text-center mb-20">
