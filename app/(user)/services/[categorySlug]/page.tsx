7bdebd1e636dc4bb748bcf8a8e349951ed9b5257
navaskm <navaskm228@gmail.com>
2026-03-20 02:15:07 -0700

    add animations in city home page

--- app/(user)/services/[categorySlug]/page.tsx
+++ app/(user)/services/[categorySlug]/page.tsx
@@ -1,9 +1,11 @@
 "use client";
 
-import React, { useEffect, useState } from "react";
+import { useEffect, useState } from "react";
 import { ArrowLeft, ArrowRight } from "lucide-react";
 import { useParams, useRouter } from "next/navigation";
 import toast from "react-hot-toast";
+import Link from "next/link";
+import { motion } from "framer-motion";
 
 import { getServiceBySlug } from "@/lib/api/public/subservices.api";
 import { getSubServicesByService } from "@/lib/api/public/subservices.api";
@@ -83,6 +85,7 @@ export default function CategoryPage() {
         {/* Back Button */}
         <button
           onClick={() => router.back()}
+           aria-label="Go back to blog page"
           className="flex items-center text-gray-600 hover:text-gray-900 mb-8"
         >
           <ArrowLeft className="w-4 h-4 mr-2" />
@@ -91,9 +94,17 @@ export default function CategoryPage() {
 
         {/* Header */}
         <div className="flex justify-between items-start mb-12">
-          <h1 className="text-5xl font-bold text-gray-800 leading-tight max-w-md">
+          <motion.h1
+            initial={{ x: -150, opacity: 0 }}
+            animate={{ x: 0, opacity: 1 }}
+            transition={{
+              duration: 2,
+              ease: [0.25, 0.1, 0.25, 1],
+            }}
+            className="text-5xl font-bold text-gray-800 leading-tight max-w-md"
+          >
             {service.title}
-          </h1>
+          </motion.h1>
 
           <div className="text-right">
             <div className="text-sm text-gray-500">All</div>
@@ -105,11 +116,19 @@ export default function CategoryPage() {
         {/* Intro */}
         <div className="flex justify-end mb-16">
           <div className="max-w-xs">
-            <p className="text-gray-500 text-sm leading-relaxed">
+            <motion.p
+              initial={{ x: 150, opacity: 0 }}
+              animate={{ x: 0, opacity: 1 }}
+              transition={{
+                duration: 2,
+                ease: [0.25, 0.1, 0.25, 1],
+              }}
+              className="text-gray-500 text-sm leading-relaxed"
+            >
                 Explore our {service.title} services designed to help businesses
                 establish and operate smoothly. Choose the right option that
                 fits your requirements and start your journey with confidence.
-            </p>
+            </motion.p>
           </div>
         </div>
 
@@ -127,54 +146,95 @@ export default function CategoryPage() {
                 {/* Image */}
                 <div className="flex justify-between items-center gap-4 lg:block">
                   <img
+                    loading="lazy"
                     src={cloudinaryAutoWebp(sub.thumbnail)}
-                    alt={sub.title}
+                    alt={sub.title || "Service thumbnail image"}
                     className="w-[70px] h-[90px] lg:w-[80px] lg:h-[100px] rounded-[160px] object-cover"
                   />
 
                   {/* Title (mobile next to image) */}
-                  <h2 className="lg:hidden text-lg font-semibold text-gray-900 leading-tight">
+                  <motion.h2
+                    initial={{ x: 100, opacity: 0 }}
+                    whileInView={{ x: 0, opacity: 1 }}
+                    viewport={{ once: false }}
+                    transition={{
+                      duration: 2,
+                      ease: [0.25, 0.1, 0.25, 1],
+                    }}
+                    className="lg:hidden text-lg font-semibold text-gray-900 leading-tight"
+                  >
                     {sub.title}
-                  </h2> 
+                  </motion.h2> 
 
                   {/* Arrow (mobile divice) */}
                   <div className="flex lg:hidden justify-end">
-                    <button
-                      onClick={() =>
-                        router.push(`/services/${service.slug}/${sub.slug}`)
-                      }
-                      className="w-5 h-12 rounded-[160px] border border-gray-200 shadow-sm flex items-center justify-center bg-white hover:bg-gray-50 transition-all duration-200"
+                    <Link
+                      href={`/services/${service.slug}/${sub.slug}`}
+                      aria-label={`View ${sub.title} service`}
+                      className="
+                        flex items-center justify-center
+                        w-8 h-8
+                        rounded-full
+                        border border-gray-200 shadow-sm
+                        bg-white
+                      "
                     >
-                      <ArrowRight className="w-5 h-5 text-gray-800" />
-                    </button>
+                      <ArrowRight className="w-4 h-4 sm:w-5 sm:h-5 text-gray-800" />
+                    </Link>
                   </div>
                 </div>
 
                 {/* Title (desktop) */}
                 <div className="hidden lg:block">
-                  <h2 className="text-2xl font-semibold text-gray-900 leading-tight">
+                  <motion.h2
+                    initial={{ x: 150, opacity: 0 }}
+                    whileInView={{ x: 0, opacity: 1 }}
+                    viewport={{ once: false }}
+                    transition={{
+                      duration: 2,
+                      ease: [0.25, 0.1, 0.25, 1],
+                    }}
+                    className="text-2xl font-semibold text-gray-900 leading-tight"
+                  >
                     {sub.title}
-                  </h2>
+                  </motion.h2>
                 </div>
 
                 {/* Description */}
                 <div>
-                  <p className="text-gray-500 text-sm leading-relaxed">
+                  <motion.p
+                    initial={{ x: -150, opacity: 0 }}
+                    whileInView={{ x: 0, opacity: 1 }}
+                    viewport={{ once: false }}
+                    transition={{
+                      duration: 2,
+                      ease: [0.25, 0.1, 0.25, 1],
+                    }}
+                    className="text-gray-500 text-sm leading-relaxed"
+                  >
                     {sub.shortDesc}
-                  </p>
+                  </motion.p>
                 </div>
 
                 {/* Arrow (large divice) */}
                 <div className="hidden lg:flex justify-end">
-                  <button
-                    onClick={() =>
-                      router.push(`/services/${service.slug}/${sub.slug}`)
-                    }
-                    className="w-10 h-24 lg:w-12 lg:h-32 rounded-[160px] border border-gray-200 shadow-sm flex items-center justify-center bg-white hover:bg-gray-50 transition-all duration-200"
+                  <Link
+                    href={`/services/${service.slug}/${sub.slug}`}
+                    aria-label={`View ${sub.title} service`}
+                    className="
+                      w-10 h-24 lg:w-12 lg:h-32
+                      rounded-full
+                      border border-gray-200 shadow-sm
+                      flex items-center justify-center
+                      bg-white hover:bg-gray-50
+                      transition-all duration-200
+                      active:scale-95
+                    "
                   >
                     <ArrowRight className="w-5 h-5 text-gray-800" />
-                  </button>
+                  </Link>
                 </div>
+
               </div>
             </div>
           ))}
