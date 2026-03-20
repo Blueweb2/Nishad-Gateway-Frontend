e2e1892bfe780d0cd6e3b2af6ba5967d1abe873c
navaskm <navaskm228@gmail.com>
2026-03-20 02:50:33 -0700

    set lazy loading in ksa expansion cost section

--- components/user/shared/FAQSection.tsx
+++ components/user/shared/FAQSection.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import { useState } from "react";
-import { Link, Plus, X } from "lucide-react";
+import { Plus, X } from "lucide-react";
 
 export type FAQItem = {
   question: string;
